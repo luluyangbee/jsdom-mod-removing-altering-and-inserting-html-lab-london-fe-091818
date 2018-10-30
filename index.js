2d856cdc43f13c552e0de051016d2e775bc57379
Lulu Yang <lulu.yang@wework.com>
2018-10-30 22:35:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /* Enter the code to remove the main node element under this comment */
 
+
+.remove ('main');
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+var element = document.createElement('H1');
+var h1 = 'victory';
+element.innerHTML = 'Lulu is the champion';
+
